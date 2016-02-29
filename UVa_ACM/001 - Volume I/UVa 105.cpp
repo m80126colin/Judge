aa564487bb9a48d9d24ce3c738abb0029025a1b8
m80126colin <m80126colin@gmail.com>
2016-03-01 01:19:53 +0800

    Add problems

--- UVa_ACM/001 - Volume I/UVa 105.cpp
+++ UVa_ACM/001 - Volume I/UVa 105.cpp
@@ -1,15 +1,25 @@
 #include <iostream>
+#include <cstdio>
+#include <cstring>
 using namespace std;
 
+#define N 10010
+int a[N];
+
 int main() {
-	int n[10000],i,j,h;
-	memset(n,0,sizeof(n));
-	while (cin>>i>>h>>j) {
-		for (i;i<j;i++) if (h>n[i]) n[i]=h;
+	int i, j, h;
+	memset(a, 0, sizeof(a));
+	while (scanf("%d%d%d", &i, &h, &j) != EOF)
+	{
+		for (i; i < j; i++)
+			if (h > a[i])
+				a[i] = h;
 	}
-	cout<<1<<" "<<n[1];
-	for (i=2;i<10000;i++) {
-		if (n[i]!=n[i-1]) cout<<" "<<i<<" "<<n[i];
+	printf("1 %d", a[1]);
+	for (i = 2; i < 10000; i++)
+	{
+		if (a[i] != a[i - 1])
+			printf(" %d %d", i, a[i]);
 	}
-	cout<<endl;
+	puts("");
 }
